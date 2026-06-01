865183a063b53f3ca12a44ce670c30690677533c
HuyNguyxn <huynguyendang77@gmail.com>
2026-06-01 21:46:23 +0700

    controller Category

---
+++ new_toy_store/src/main/java/com/example/new_toy_store/category/api/CategoryController.java
@@ -0,0 +1,45 @@
+package com.example.new_toy_store.category.api;
+
+import com.example.new_toy_store.category.application.CategoryService;
+import com.example.new_toy_store.category.application.dto.request.CategoryRequest;
+import com.example.new_toy_store.category.application.dto.response.CategoryResponse;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService service;
+
+    public CategoryController(CategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public Page<CategoryResponse> getAllCategories(Pageable pageable) {
+        return service.getAllCategories(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponse getCategory(@PathVariable Integer id) {
+        return service.getCategory(id);
+    }
+
+    @PostMapping
+    public CategoryResponse create(@Valid @RequestBody CategoryRequest request) {
+        return service.create(request);
+    }
+
+    @PutMapping("/{id}")
+    public CategoryResponse update(@PathVariable Integer id, @Valid @RequestBody CategoryRequest request) {
+        return service.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        service.delete(id);
+    }
+}
