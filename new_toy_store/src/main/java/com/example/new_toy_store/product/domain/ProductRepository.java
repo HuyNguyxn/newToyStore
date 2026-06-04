8baa431ff0deef2c5a77a6592e8458a735e7fb89
HuyNguyxn <huynguyendang77@gmail.com>
2026-06-04 20:31:00 +0700

    them query find detail o ProductRepository

--- new_toy_store/src/main/java/com/example/new_toy_store/product/domain/ProductRepository.java
+++ new_toy_store/src/main/java/com/example/new_toy_store/product/domain/ProductRepository.java
@@ -5,11 +5,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import java.util.List;
+import java.util.Set;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     @Query("SELECT p FROM Product p LEFT JOIN FETCH p.images LEFT JOIN FETCH p.variants WHERE p.id = :id")
     Product findByIdWithDetails(@Param("id") Integer id);
+
+    @Query("SELECT DISTINCT p FROM Product p LEFT JOIN FETCH p.images LEFT JOIN FETCH p.variants WHERE p.id IN :ids")
+    List<Product> findAllByIdsWithDetails(@Param("ids") Set<Integer> ids);
 
     Page<Product> findByCategoryId(Integer categoryId, Pageable pageable);
 }
