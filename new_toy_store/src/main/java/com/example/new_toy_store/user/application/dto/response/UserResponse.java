03b362d4ec8b3db71894f3d46ef8bac18cf3f6dd
HuyNguyxn <huynguyendang77@gmail.com>
2026-06-09 10:48:02 +0700

    DTO user

---
+++ new_toy_store/src/main/java/com/example/new_toy_store/user/application/dto/response/UserResponse.java
@@ -0,0 +1,35 @@
+package com.example.new_toy_store.user.application.dto.response;
+
+import java.util.List;
+
+public class UserResponse {
+
+    private Integer id;
+    private String email;
+    private String fullName;
+    private String phoneNumber;
+    private String avatarUrl;
+    private String role;
+    private String status;
+    private List<AddressResponse> addresses;
+
+    public UserResponse(Integer id, String email, String fullName, String phoneNumber, String avatarUrl, String role, String status, List<AddressResponse> addresses) {
+        this.id = id;
+        this.email = email;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.avatarUrl = avatarUrl;
+        this.role = role;
+        this.status = status;
+        this.addresses = addresses;
+    }
+
+    public Integer getId() { return id; }
+    public String getEmail() { return email; }
+    public String getFullName() { return fullName; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public String getAvatarUrl() { return avatarUrl; }
+    public String getRole() { return role; }
+    public String getStatus() { return status; }
+    public List<AddressResponse> getAddresses() { return addresses; }
+}
